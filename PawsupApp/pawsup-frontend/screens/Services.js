1f63eb6b68cf94e1fc1455e6935fbedb3921641f
YaySushi <ducklingdood@gmail.com>
2021-10-21 23:56:28 -0400

    [PLSGIB-29] Add Views for filtering

--- PawsupApp/pawsup-frontend/screens/Services.js
+++ PawsupApp/pawsup-frontend/screens/Services.js
@@ -1,31 +1,13 @@
-import React from 'react';
-import { SafeAreaView, TouchableOpacity, ImageBackground, View, FlatList, StyleSheet, Text, StatusBar, Image, Dimensions, Button } from 'react-native';
+import React, { useState } from 'react';
+import {Picker} from '@react-native-picker/picker';
+import { SafeAreaView, TouchableOpacity, ImageBackground, View, FlatList, StyleSheet, Text, StatusBar, Image, Dimensions} from 'react-native';
 import Entry from '../components/Entry';
 
-import {
-    BackgroundStyle,
-    StyledContainer2,
-    StyledContainer,
-    InnerContainer,
-    PageTitle,
-    StyledFormArea,
-    LeftIcon,
-    RightIcon,
-    StyledInputLabel,
-    StyledTextInput,
-    StyledButton,
-    MsgBox,
-    ExtraView,
-    ExtraText,
-    TextLink,
-    TextLinkContent,
-    Colours,
-    ButtonText
-} from './../components/styles';
+import { BackgroundStyle, StyledContainer2, PageTitle, } from './../components/styles';
 
-const BG_IMG = 'pawpus.finestudiotest.com/include/pic/backend/21070708564800.png';
 const CAT_IMG = 'https://www.pethealthnetwork.com/sites/default/files/urine-testing-in-cats185922494.png';
 const FILTER_IMG = require('./../assets/icons/filter.png');
+const SORT_IMG = require('./../assets/icons/sort.png');
 
 // some data i guess?
 const DATA = [
@@ -128,6 +110,11 @@ const Services = () => {
 	const renderItem = ({ item }) => (
 		<Item title={item.title} />
 	);
+
+	const [filterVisible, setFilterVisible] = useState(false);
+	const [selectedPrice, setSelectedPrice] = useState();
+	const [selectedDistance, setSelectedDistance] = useState();
+
 	
 	return (
 		<StyledContainer2>
@@ -140,57 +127,139 @@ const Services = () => {
 
 			<StatusBar style="light" />
 			<PageTitle style={{color: 'black', marginTop: 10}}>Available Services</PageTitle>
+			{!filterVisible && 
+				<SafeAreaView style={{marginTop: 20}}>
+					<View style={{flexDirection: 'row'}}>
+						<TouchableOpacity
+							style={styles.filterButtonStyle}
+							onPress={() => {
+								setFilterVisible(!filterVisible);
+							}}
+							>
+							<Image
+								source={FILTER_IMG}
+								style={styles.buttonImageIconStyle}
+							/>
+							<Text style={styles.buttonTextStyle}>
+								FILTER & SORT
+							</Text>
+						</TouchableOpacity>
+					</View>
+				</SafeAreaView>
+			}
+			
 
-			<SafeAreaView style={{marginTop: 20}}>
-				<View style={{flexDirection: 'row'}}>
-					<TouchableOpacity style={styles.filterButtonStyle}>
-						<Image
-							source={FILTER_IMG}
-							style={styles.buttonImageIconStyle}
-						/>
-						<Text style={styles.buttonTextStyle}>
-							SORT & FILTER
+			{ filterVisible &&  
+				<SafeAreaView style={{margin: 15, alignContent: 'center'}}>
+
+					{/*	area for price filter	*/}
+					<SafeAreaView style={{marginVertical: 10, flexDirection: 'row'}}>
+						<View style={{flex: 4}}>
+							<Text style={{fontSize: 18, fontWeight: 'bold'}}>
+								Choose a price range:
+							</Text>
+						</View>
+						<View style={{flex: 3, alignContent: 'center'}}>	
+							<Picker
+								selectedValue={selectedPrice}
+								mode={'dropdown'}
+								dropdownIconColor={'red'}
+								onValueChange={(itemValue, itemIndex) => setSelectedPrice(itemValue)}
+							>
+								<Picker.Item label="Any" value="any" />
+								<Picker.Item label="Under $10" value="0,10" />
+								<Picker.Item label="$10 to $20" value="10,20" />
+								<Picker.Item label="$20 to $50" value="20,50" />
+								<Picker.Item label="$50 to $100" value="50,100" />
+								<Picker.Item label="Over $100" value="100,inf" />
+							</Picker>
+						</View>
+					</SafeAreaView>
+
+					{/* place for distance filter */}
+					<SafeAreaView style={{marginVertical: 10, flexDirection: 'row'}}>
+						<View style={{flex: 4}}>
+							<Text style={{fontSize: 18, fontWeight: 'bold'}}>
+								Choose a distance range:
+							</Text>
+						</View>
+						<View style={{flex: 3, alignContent: 'center'}}>	
+							<Picker
+								selectedValue={selectedDistance}
+								mode={'dropdown'}
+								dropdownIconColor={'red'}
+								onValueChange={(itemValue, itemIndex) => setSelectedDistance(itemValue)}
+							>
+								<Picker.Item label="Any" value="any" />
+								<Picker.Item label="Under 1km" value="0,10" />
+								<Picker.Item label="1km - 2km" value="10,20" />
+								<Picker.Item label="2km - 5km" value="20,50" />
+								<Picker.Item label="5km - 10km" value="50,100" />
+								<Picker.Item label="10km - 20km" value="100,inf" />
+								<Picker.Item label="Over 20km" value="100,inf" />
+							</Picker>
+						</View>
+					</SafeAreaView>
+
+					<TouchableOpacity
+						style={{
+							marginTop: 30,
+							flexDirection: 'row',
+							backgroundColor: 'rgba(255, 255, 255, 0.5)',
+							borderWidth: 3,
+							borderColor: '#000',
+							width: WIDTH / 2 - 30,
+							height: 50,
+							borderRadius: 10,
+							alignSelf: 'center',
+						}}
+						onPress={() => {
+							setFilterVisible(!filterVisible);
+						}}
+					>
+						<Text style={{
+							fontSize: 20,
+							alignSelf: 'center',
+							color: '#000',
+							flex: 1,
+							textAlign: 'center',
+						}}>
+							DONE
 						</Text>
 					</TouchableOpacity>
-			{/*
-					<TouchableOpacity style={styles.filterButtonStyle}>
-					<Image
-						source={FILTER_IMG}
-						style={styles.buttonImageIconStyle}
-					/>
-					<Text style={styles.buttonTextStyle}>
-						Filter
-					</Text>
-				</TouchableOpacity>
-			*/}
-				</View>
-			</SafeAreaView>
+				</SafeAreaView>
+			}
 
-			<SafeAreaView style={styles.container}>
-				<FlatList
-					data={DATA}
-					style={{ flex: 1 }}
-					contentContainerStyle={{
-						padding: SPACING
-					}}
-					columnWrapperStyle={{
-						justifyContent: 'space-between',
-						marginBottom: 15,
-					}}
-					numColumns={2}
-					renderItem={({item, index}) => {
-						return <View>
-							<Entry item={item} />
-						</View>
-					}}
-					keyExtractor={item => item.id}
-				/>
-			</SafeAreaView>
+			{ !filterVisible &&
+				<SafeAreaView style={styles.container}>
+					<FlatList
+						data={DATA}
+						style={{ flex: 1 }}
+						contentContainerStyle={{
+							padding: SPACING
+						}}
+						columnWrapperStyle={{
+							justifyContent: 'space-between',
+							marginBottom: 15,
+						}}
+						numColumns={2}
+						renderItem={({item, index}) => {
+							return <View>
+								<Entry item={item} />
+							</View>
+						}}
+						keyExtractor={item => item.id}
+					/>
+				</SafeAreaView>
+			}
 		</StyledContainer2>
 	);
 }
 
 const styles = StyleSheet.create({
+	dropdownstyle: {
+		backgroundColor: 'black',
+	},
 	container: {
 		paddingHorizontal: 5,
 		flex: 1,
@@ -211,11 +280,11 @@ const styles = StyleSheet.create({
 	filterButtonStyle: {
 		marginLeft: 25,
 		flexDirection: 'row',
-		backgroundColor: 'rgba(255, 255, 255, 0.7)',
-		borderWidth: 2,
+		backgroundColor: 'rgba(255, 255, 255, 0)',
+		borderWidth: 0,
 		borderColor: '#000',
 		width: WIDTH / 2 - 40,
-		height: 30,					 /* THIS IS A FIXED VALUE. CHANGE LATER??? */
+		height: 25,					 /* THIS IS A FIXED VALUE. CHANGE LATER??? */
 		borderRadius: 10,
 	},
 	buttonImageIconStyle: {
@@ -226,7 +295,7 @@ const styles = StyleSheet.create({
 	buttonTextStyle: {
 		fontSize: 15,
 		alignSelf: 'center',
-		marginLeft: 5,
+		marginLeft: 2,
 		color: '#000',
 		flex: 1,
 	},
